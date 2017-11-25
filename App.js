8c7472eb0cc54db63905aac17c288ec7d543e804
Mahmoud Slah <mahmoudslah25@gmail.com>
2017-11-25 17:07:17 +0200

    home screen

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { AppLoading, Asset, Font } from 'expo';
 
-import { StyleSheet, Text, View, Image, StatusBar, ImageBackground, TouchableNativeFeedback } from 'react-native';
+import { StyleSheet, Text, View, Image, StatusBar, ImageBackground, TouchableHighlight, Dimensions, Platform, PixelRatio } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 import Swiper from 'react-native-swiper';
 
+var {height, width} = Dimensions.get('window');
 
 function cacheImages(images) {
   return images.map(image => {
@@ -52,7 +53,39 @@ export default class App extends React.Component {
             <Swiper 
               style={styles.wrapper} 
               dotColor="rgb(94,96,100)"
-              activeDotColor="rgb(252,252,252)">
+              activeDotColor="rgb(252,252,252)"
+              loop={false}
+              index={4}>
+              
+        <Image source={require('./assets/back-mdpi.png')} style={styles.slide1}>
+          <Text style={styles.text}>Hello Swiper</Text>
+        </Image>
+        <Image source={require('./assets/back2-mdpi.png')} style={styles.slide1}>
+          <View style={styles.trapezoid}>
+          <Text>تواصل معنا</Text>
+          <Text>تواصل معنا</Text>
+          <Text>تواصل معنا</Text>
+          <Text>تواصل معنا</Text>
+          </View>
+        </Image>
+        <Image source={require('./assets/back2-mdpi.png')} style={styles.slide2}>
+          <Image source={require('./assets/logo.png')} style={[styles.logo, {alignSelf: "center", marginRight: 0}]} resizeMode="contain"/>
+          <View style={styles.textBack}>
+            <Text style={styles.text}>
+            هذا النص هو مثال لنص يمكن أن يستبدل في نفس المساحة، لقد تم توليد هذا النص من مولد النص العربى، حيث يمكنك أن تولد مثل هذا النص أو العديد من النصوص الأخرى إضافة إلى زيادة عدد الحروف التى يولدها التطبيق.
+            </Text>
+          </View>
+          <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+
+          <TouchableHighlight style={[styles.btn4, {backgroundColor: '#ea3c3c'}]}>
+              <Text style={styles.btnText}>تواصل معنا</Text>
+          </TouchableHighlight>
+
+          <TouchableHighlight style={styles.btn4}>
+              <Text style={styles.btnText}>انضم الينا الان</Text>
+          </TouchableHighlight>
+          </View>
+        </Image>
         <Image  source={require('./assets/main-bg-mdpi.png')} style={styles.slide1}>
           <Image source={require('./assets/logo.png')} style={styles.logo} resizeMode="contain"/>
           <Text style={{alignSelf: "flex-end", fontFamily: 'NeoSansArabic', fontSize: 20, marginRight: 20,}}>
@@ -64,45 +97,15 @@ export default class App extends React.Component {
           <Text style={{color: "#ffffff", alignSelf: "flex-end", fontFamily: 'NeoSansArabic', fontSize: 14, marginRight: 20,}}>
           هل أنت مستعد للتجربة
           </Text>
-          <TouchableNativeFeedback>
-            <View style={styles.btn1}>
+          <TouchableHighlight style={styles.btn1}>
               <Text style={styles.btnText}>انضم الينا الان</Text>
-            </View>
-          </TouchableNativeFeedback>
-          <TouchableNativeFeedback>
-            <View style={styles.btn2}>
+          </TouchableHighlight>
+          <TouchableHighlight style={styles.btn2}>
               <Text style={[styles.btnText, {padding: 19}]}>تعرف على مركز بودي تيك</Text>
-            </View>
-          </TouchableNativeFeedback>
-          <TouchableNativeFeedback>
-            <View style={styles.btn3}>
+          </TouchableHighlight>
+          <TouchableHighlight style={styles.btn3}>
               <Text style={[styles.btnText, {padding: 20, fontSize: 18}]}>تواصل معنا</Text>
-            </View>
-          </TouchableNativeFeedback>
-        </Image>
-        <Image source={require('./assets/back2-mdpi.png')} style={styles.slide2}>
-          <Image source={require('./assets/logo.png')} style={[styles.logo, {alignSelf: "center", marginRight: 0,}]} resizeMode="contain"/>
-          <Text style={[styles.text, {}]}>
-          هذا النص هو مثال لنص يمكن أن يستبدل في نفس المساحة، لقد تم توليد هذا النص من مولد النص العربى، حيث يمكنك أن تولد مثل هذا النص أو العديد من النصوص الأخرى إضافة إلى زيادة عدد الحروف التى يولدها التطبيق.
-          </Text>
-          <View style={{backgroundColor: '#ea3c3c',  flexDirection: 'row', justifyContent: 'space-between'}}>
-          <TouchableNativeFeedback>
-            <View style={[styles.btn4, {backgroundColor: '#ea3c3c'}]}>
-              <Text style={styles.btnText}>تواصل معنا</Text>
-            </View>
-          </TouchableNativeFeedback>
-          <TouchableNativeFeedback>
-            <View style={styles.btn4}>
-              <Text style={styles.btnText}>انضم الينا الان</Text>
-            </View>
-          </TouchableNativeFeedback>
-          </View>
-        </Image>
-        <Image source={require('./assets/back-mdpi.png')} style={styles.slide1}>
-          <Text style={styles.text}>Hello Swiper</Text>
-        </Image>
-        <Image source={require('./assets/back2-mdpi.png')} style={styles.slide1}>
-          <Text style={styles.text}>Hello Swiper</Text>
+          </TouchableHighlight>
         </Image>
       </Swiper>
     );
@@ -113,11 +116,24 @@ var styles = StyleSheet.create({
   wrapper: {
     
   },
+  trapezoid: {
+    width: 300,
+    height: 0,
+    borderBottomWidth: 200,
+    borderBottomColor: '#0584f7',
+    borderLeftWidth: 10,
+    borderLeftColor: 'transparent',
+    borderStyle: 'solid',
+    alignSelf: "flex-end",
+  },
   btnText: {
     fontFamily: 'NeoSansArabic',
     color: 'white',
     textAlign: 'center',
     padding: 8,
+  },
+  textBack: {
+    backgroundColor: 'rgba(0,0,0,0)'
   },
   slide1: {
     flex: 1,
@@ -130,7 +146,8 @@ var styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'space-around',
     alignItems: 'center',
-    backgroundColor: '#97CAE5',
+    width: null,
+    height: null,
   },
   slide3: {
     flex: 1,
@@ -141,9 +158,9 @@ var styles = StyleSheet.create({
   text: {
     color: '#fff',
     fontFamily: 'NeoSansArabic',
-    fontSize: 14,
     textAlign: 'center',
-    padding: 20,
+    padding: 15,
+    fontSize: 14,
     lineHeight: 30
   },
   logo: {
@@ -154,17 +171,18 @@ var styles = StyleSheet.create({
   },
   btn1: {
     backgroundColor: '#0584f7',
-    width: 160,
-    height: 40,
-    borderRadius: 100,
+    paddingRight:40,
+    paddingLeft:40,
+    paddingTop: 5,
+    paddingBottom: 5,
+    borderRadius:100,
     alignSelf: "flex-end",
-    marginRight: 20,
+    marginRight: 10,
     marginTop: 20,
   },
   btn2: {
     backgroundColor: '#ea3c3c',
-    width: 200,
-    height: 60,
+    overflow: 'hidden',
     borderTopLeftRadius : 100,
     borderBottomLeftRadius : 100,
     alignSelf: "flex-end",
@@ -173,17 +191,22 @@ var styles = StyleSheet.create({
   btn3: {
     
     backgroundColor: '#373838',
-    width: 180,
-    height: 70,
+    overflow: 'hidden',
     borderTopLeftRadius : 100,
     borderBottomLeftRadius : 100,
     alignSelf: "flex-end",
     marginTop: 20,
+    paddingRight:20,
+    paddingLeft:20,
   },
   btn4: {
     backgroundColor: '#0584f7',
-    width: 160,
-    height: 40,
     borderRadius: 100,
+    overflow: 'hidden',
+    paddingTop:5,
+    paddingBottom:5,
+    paddingRight:25,
+    paddingLeft:25,
+    margin: 10
   }
 })
