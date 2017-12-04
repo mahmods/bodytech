65ff43a5600d2e4ed58725e01b37d1255f10e4cf
Mahmoud Slah <mahmoudslah25@gmail.com>
2017-12-04 16:57:11 +0200

    m

--- screens/about/about.js
+++ screens/about/about.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { WebView, Text,RefreshControl,ActivityIndicator,ScrollView,KeyboardAvoidingView, ListView, TextInput, View, Image, StatusBar, TouchableHighlight, Dimensions, Platform, PixelRatio } from 'react-native';
+import { WebView,ImageBackground, Text,RefreshControl,ActivityIndicator,ScrollView,KeyboardAvoidingView, ListView, TextInput, View, Image, StatusBar, TouchableHighlight, Dimensions, Platform, PixelRatio } from 'react-native';
 import Swiper from 'react-native-swiper';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
 import Lightbox from 'react-native-lightbox';
@@ -71,34 +71,53 @@ export default class about extends React.Component {
           }
         return (
             <View style={styles.backGround}>
-            <ScrollView>
-            <Swiper style={{width: WINDOW_WIDTH, height: WINDOW_HEIGHT * 0.3, marginBottom:20}}>
-            {this.state.dataSource.map(function(object, i){
-              return (
-              <Lightbox key={i} navigator={this.navigation} activeProps={{resizeMode: "contain"}}>
-              <Image resizeMode="cover" style={{width: '100%', height: '100%'}} defaultSource={{uri: object.image_url.default_image}} source={{uri: object.image_url.ful_url}}/>
-              </Lightbox>);
-            })} 
-            </Swiper>
-            <Text style={[styles.text, {fontSize: scale(20)}]}>من نحن</Text>
-            <Text  style={[styles.text, {lineHeight: scale(40)}]}>قاعه بدي تك لبناء الاجسام , فتنس , رشاقه واعاده تأهيل المصابين باستخدام احدث تقنيه المانيه تعتمد على التحفيز الكهربائي للعضلات ...... فقط 20 دقيقه مرتان اسبوعيا </Text>
-            <WebView
-                style={{width: WINDOW_WIDTH, height: WINDOW_HEIGHT * 0.3}}
-                javaScriptEnabled={true}
-                source={{uri: 'https://www.youtube.com/embed/hxqr8IT7uDs?rel=0&autoplay=0&showinfo=0'}}
-            />
-            <Text style={[styles.text, {fontSize: scale(20)}]}>لا وقت للتمارين اليومية ؟ </Text>
-            <View style={{flexDirection: 'row', justifyContent: 'center'}}>
-              <Text style={[styles.text, {color:'#0584f7', fontSize: scale(20)}]}> هو الحل </Text>
-              <Text style={[styles.text, {color:'#ee3b30', fontSize: scale(20)}]}>Body Tec</Text>
+            
+          <Image source={require('../../assets/back2-mdpi.png')} style={styles.slide4}>
+            <View style={[styles.textBack, {paddingRight: 5, paddingLeft: 5}]}>
+              <Text style={[styles.text, {alignSelf:'flex-end', textAlign: 'right', fontSize: scale(22), paddingBottom: 0}]}>عنوان السلايدر</Text>
+              <Text style={[styles.text, {textAlign: 'right', paddingTop: 0}]}>
+              قاعه بدي تك لبناء الاجسام , فتنس , رشاقه واعاده تأهيل المصابين باستخدام احدث تقنيه المانيه تعتمد على التحفيز الكهربائي للعضلات ...... فقط 20 دقيقه مرتان اسبوعيا 
+              </Text>
             </View>
-            <Text style={styles.text}>تدريب الجسم</Text>
-            <Text style={styles.text}>20 دقيقة تجريب الدورة في الأسبوع</Text>
-            <Text style={styles.text}>نتائج اللياقة البدنية آمنة وفعالة</Text>
-            <Text style={styles.text}>خطة التغذية الصحية مصممة خصيصا لمتطلباتك</Text>
-            <Text style={styles.text}>نتائج اللياقة البدنية آمنة وفعالة</Text>
-            <Text style={styles.text}>تدريب كامل مكثف للجسم</Text>
-            </ScrollView>
+            <ImageBackground source={require('../../assets/contentBg.png')} style={{alignItems: 'flex-end', alignSelf:'flex-end', marginTop: 30, width: Dimensions.get('window').width * 0.8}}>
+              <View style={{marginTop: 10, marginRight: 10}}>
+                <View style={styles.pathInnerWrapper}>
+                  <Text style={styles.pathText}>تدريب الجسم</Text>
+                  <Ionicons name="ios-checkmark-circle-outline" size={scale(20)} color="#fff"/>
+                </View>
+                <View style={styles.pathInnerWrapper}>
+                  <Text style={styles.pathText}>20 دقيقة تجريب الدورة في الأسبوع</Text>
+                  <Ionicons name="ios-checkmark-circle-outline" size={scale(20)} color="#fff"/>
+                </View>
+                <View style={styles.pathInnerWrapper}>
+                  <Text style={styles.pathText}>نتائج اللياقة البدنية آمنة وفعالة</Text>
+                  <Ionicons name="ios-checkmark-circle-outline" size={scale(20)} color="#fff"/>
+                </View>
+                <View style={styles.pathInnerWrapper}>
+                  <Text style={styles.pathText}>خطة التغذية الصحية مصممة خصيصا لمتطلباتك</Text>
+                  <Ionicons name="ios-checkmark-circle-outline" size={scale(20)} color="#fff"/>
+                </View>
+                <View style={styles.pathInnerWrapper}>
+                  <Text style={styles.pathText}>نتائج اللياقة البدنية آمنة وفعالة</Text>
+                  <Ionicons name="ios-checkmark-circle-outline" size={scale(20)} color="#fff"/>
+                </View>
+                <View style={styles.pathInnerWrapper}>
+                  <Text style={styles.pathText}>تدريب كامل مكثف للجسم</Text>
+                  <Ionicons name="ios-checkmark-circle-outline" size={scale(20)} color="#fff"/>
+                </View>
+              </View>
+             </ImageBackground>
+
+             <View style={{flexDirection: 'row', justifyContent: 'space-between', marginTop: 40}}>
+              <TouchableHighlight onPress={() => this.props.navigation.navigate('Join')} style={styles.btn4}>
+                  <Text style={styles.btnText}>انضم الينا الان</Text>
+              </TouchableHighlight>
+              <TouchableHighlight onPress={() => this.props.navigation.navigate('Contact')} style={[styles.btn4, {backgroundColor: '#ea3c3c'}]}>
+                  <Text style={styles.btnText}>تواصل معنا</Text>
+              </TouchableHighlight>
+              </View>
+
+          </Image>
             </View>
         )
     }
@@ -117,11 +136,49 @@ const styles = StyleSheet.create({
       flex: 1,
       height: WINDOW_WIDTH * 0.41,
     },
+    pathText: {
+      color: '#fff',
+      fontSize: scale(10),
+      fontFamily: 'NeoSansArabic',
+      marginRight: 5
+    },
+    pathInnerWrapper: {
+      justifyContent: 'flex-end',
+      flexDirection:'row',
+      marginBottom: 10,
+    },
     text: {
-        color: '#fff',
-        textAlign: 'center',
-        fontFamily: 'NeoSansArabic',
-        margin: 10,
-        fontSize: scale(12)
+      color: '#fff',
+      fontFamily: 'NeoSansArabic',
+      textAlign: 'center',
+      padding: 15,
+      fontSize: scale(12),
+      lineHeight: scale(30)
+    },
+    textBack: {
+      backgroundColor: 'rgba(0,0,0,0)'
+    },
+    slide4: {
+      flex: 1,
+      justifyContent: 'flex-end',
+      alignItems: 'center',
+      paddingBottom: 50,
+      width: null,
+      height: null,
+    },
+    btnText: {
+      fontFamily: 'NeoSansArabic',
+      color: 'white',
+      textAlign: 'center',
+      padding: 8,
+      fontSize: scale(10)
+    },
+    btn4: {
+      backgroundColor: '#0584f7',
+      borderRadius: 100,
+      overflow: 'hidden',
+      paddingRight:scale(35),
+      paddingLeft:scale(35),
+      margin: 10,
     }
   })
--- screens/home/home.js
+++ screens/home/home.js
@@ -28,119 +28,7 @@ export default class Home extends React.Component {
       });
     render() {
       return (
-        <Swiper 
-          style={styles.wrapper} loop={false} index={4}
-          dotColor="rgb(94,96,100)" activeDotColor="rgb(252,252,252)">
-
-          <Image source={require('../../assets/back-mdpi.png')} style={styles.slide1}>
-            <View style={styles.textBack}>
-              <Text style={[styles.text, {alignSelf:'flex-end', textAlign: 'right', fontSize: scale(22), paddingBottom: 0}]}>عنوان السلايدر</Text>
-              <Text style={[styles.text, {textAlign: 'right', paddingTop: 0}]}>
-              هذا النص هو مثال لنص يمكن أن يستبدل في نفس المساحة، لقد تم توليد هذا النص من مولد النص العربى، حيث يمكنك أن تولد مثل هذا النص أو العديد من النصوص الأخرى إضافة إلى زيادة عدد الحروف التى يولدها التطبيق.
-              </Text>
-            </View>
-
-            <ImageBackground style={{marginBottom: 10, alignSelf: 'flex-start',flexDirection: 'row', width: Dimensions.get('window').width * 0.95, padding: 15}} source={require('../../assets/content1.png')}>
-              <View style={{flex: 1, flexDirection: 'row'}}>
-                <Text style={[styles.pathText]}>هذا النص هو مثال لنص يمكن أن يستبدل في نفس المساحة</Text>
-                <Ionicons name="ios-checkmark-circle-outline" size={scale(20)} color="#fff"/>
-              </View>
-            </ImageBackground>
-
-            <ImageBackground style={{marginBottom: 10, alignSelf: 'flex-end',flexDirection: 'row', width: Dimensions.get('window').width * 0.95, padding: 15}} source={require('../../assets/content2.png')}>
-              <View style={{flex: 1, flexDirection: 'row', justifyContent:'flex-end'}}>
-                <Text style={[styles.pathText, {textAlign: 'right'}]}>هذا النص هو مثال لنص يمكن أن يستبدل في نفس المساحة</Text>
-                <Ionicons name="ios-checkmark-circle-outline" size={scale(20)} color="#fff"/>
-              </View>
-            </ImageBackground>
-
-            <ImageBackground style={{marginBottom: 10, alignSelf: 'flex-start',flexDirection: 'row', width: Dimensions.get('window').width * 0.95, padding: 15}} source={require('../../assets/content1.png')}>
-              <View style={{flex: 1, flexDirection: 'row'}}>
-                <Text style={[styles.pathText, {textAlign: 'left'}]}>هذا النص هو مثال لنص يمكن أن يستبدل في نفس المساحة</Text>
-                <Ionicons name="ios-checkmark-circle-outline" size={scale(20)} color="#fff"/>
-              </View>
-            </ImageBackground>
-
-            <ImageBackground style={{marginBottom: 10, alignSelf: 'flex-end',flexDirection: 'row', width: Dimensions.get('window').width * 0.95, padding: 15}} source={require('../../assets/content2.png')}>
-              <View style={{flex: 1, flexDirection: 'row', justifyContent:'flex-end'}}>
-                <Text style={[styles.pathText, {textAlign: 'right'}]}>هذا النص هو مثال لنص يمكن أن يستبدل في نفس المساحة</Text>
-                <Ionicons name="ios-checkmark-circle-outline" size={scale(20)} color="#fff"/>
-              </View>
-            </ImageBackground>
-
-            <View style={{flexDirection: 'row', justifyContent: 'space-between', marginTop: 20}}>
-              <TouchableHighlight onPress={() => this.props.navigation.navigate('Join')} style={styles.btn4}>
-                  <Text style={styles.btnText}>انضم الينا الان</Text>
-              </TouchableHighlight>
-              <TouchableHighlight onPress={() => this.props.navigation.navigate('Contact')} style={[styles.btn4, {backgroundColor: '#ea3c3c'}]}>
-                  <Text style={styles.btnText}>تواصل معنا</Text>
-              </TouchableHighlight>
-            </View>
-          </Image>
-
-          <Image source={require('../../assets/back2-mdpi.png')} style={styles.slide4}>
-            <View style={[styles.textBack, {paddingRight: 5, paddingLeft: 5}]}>
-              <Text style={[styles.text, {alignSelf:'flex-end', textAlign: 'right', fontSize: scale(22), paddingBottom: 0}]}>عنوان السلايدر</Text>
-              <Text style={[styles.text, {textAlign: 'right', paddingTop: 0}]}>
-              هذا النص هو مثال لنص يمكن أن يستبدل في نفس المساحة، لقد تم توليد هذا النص من مولد النص العربى، حيث يمكنك أن تولد مثل هذا النص أو العديد من النصوص الأخرى إضافة إلى زيادة عدد الحروف التى يولدها التطبيق.
-              </Text>
-            </View>
-            <ImageBackground source={require('../../assets/contentBg.png')} style={{alignItems: 'flex-end', alignSelf:'flex-end', marginTop: 30, width: Dimensions.get('window').width * 0.95}}>
-              <View style={{marginTop: 10, marginRight: 10}}>
-                <View style={styles.pathInnerWrapper}>
-                  <Text style={styles.pathText}>هذا النص هو مثال لنص يمكن أن يستبدل في نفس المساحة</Text>
-                  <Ionicons name="ios-checkmark-circle-outline" size={scale(20)} color="#fff"/>
-                </View>
-                <View style={styles.pathInnerWrapper}>
-                  <Text style={styles.pathText}>هذا النص هو مثال لنص يمكن أن يستبدل في نفس المساحة</Text>
-                  <Ionicons name="ios-checkmark-circle-outline" size={scale(20)} color="#fff"/>
-                </View>
-                <View style={styles.pathInnerWrapper}>
-                  <Text style={styles.pathText}>هذا النص هو مثال لنص يمكن أن يستبدل في نفس المساحة</Text>
-                  <Ionicons name="ios-checkmark-circle-outline" size={scale(20)} color="#fff"/>
-                </View>
-                <View style={styles.pathInnerWrapper}>
-                  <Text style={styles.pathText}>هذا النص هو مثال لنص يمكن أن يستبدل في نفس</Text>
-                  <Ionicons name="ios-checkmark-circle-outline" size={scale(20)} color="#fff"/>
-                </View>
-                <View style={styles.pathInnerWrapper}>
-                  <Text style={styles.pathText}>هذا النص هو مثال لنص يمكن أن يستبدل في نفس</Text>
-                  <Ionicons name="ios-checkmark-circle-outline" size={scale(20)} color="#fff"/>
-                </View>
-              </View>
-             </ImageBackground>
-
-             <View style={{flexDirection: 'row', justifyContent: 'space-between', marginTop: 40}}>
-              <TouchableHighlight onPress={() => this.props.navigation.navigate('Join')} style={styles.btn4}>
-                  <Text style={styles.btnText}>انضم الينا الان</Text>
-              </TouchableHighlight>
-              <TouchableHighlight onPress={() => this.props.navigation.navigate('Contact')} style={[styles.btn4, {backgroundColor: '#ea3c3c'}]}>
-                  <Text style={styles.btnText}>تواصل معنا</Text>
-              </TouchableHighlight>
-              </View>
-
-          </Image>
-
-          <Image source={require('../../assets/back2-mdpi.png')} style={styles.slide2}>
-            <Image source={require('../../assets/logo.png')} style={[styles.logo, {alignSelf: "center", marginRight: 0}]} resizeMode="contain"/>
-            <View style={styles.textBack}>
-              <Text style={styles.text}>
-              هذا النص هو مثال لنص يمكن أن يستبدل في نفس المساحة، لقد تم توليد هذا النص من مولد النص العربى، حيث يمكنك أن تولد مثل هذا النص أو العديد من النصوص الأخرى إضافة إلى زيادة عدد الحروف التى يولدها التطبيق.
-              </Text>
-            </View>
-
-            <View style={{flexDirection: 'row', justifyContent: 'space-between', marginTop: Dimensions.get('window').height * 0.25}}>
-            <TouchableHighlight onPress={() => this.props.navigation.navigate('Join')} style={styles.btn4}>
-                <Text style={styles.btnText}>انضم الينا الان</Text>
-            </TouchableHighlight>
-              <TouchableHighlight onPress={() => this.props.navigation.navigate('Contact')} style={[styles.btn4, {backgroundColor: '#ea3c3c'}]}>
-                <Text style={styles.btnText}>تواصل معنا</Text>
-            </TouchableHighlight>
-            </View>
-
-          </Image>
-
-          <Image source={require('../../assets/main-bg.png')} style={[styles.slide1]}>
+            <Image source={require('../../assets/main-bg.png')} style={[styles.slide1]}>
             <Image source={require('../../assets/logo.png')} style={[styles.logo, {width: scale(250)}]} resizeMode="contain"/>
             <Text style={{alignSelf: "flex-end", fontFamily: 'NeoSansArabic', fontSize: scale(22), marginRight: 20,}}>
               <Text style={{color: "#1e94ff"}}>أبني </Text>
@@ -161,7 +49,6 @@ export default class Home extends React.Component {
                 <Text style={[styles.btnText, {padding: 20, fontSize: scale(16)}]}>تواصل معنا</Text>
             </TouchableHighlight>
           </Image>
-        </Swiper>
       );
     }
   }
